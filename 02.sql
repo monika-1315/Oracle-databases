1c6ea12c4f12fb3fe26d9898e737374348119196
Monika Marusiak <moniusiar@gmail.com>
2019-11-01 21:10:08 +0100

    trying to do 33b

--- 02.sql
+++ 02.sql
@@ -320,4 +320,49 @@ UNION ALL
         to_char(SUM((NVL(przydzial_myszy,0)+NVL(myszy_extra,0)))) "SUMA"
 FROM Kocury));
 
+
 --b
+
+SELECT nazwa_bandy "NAZWA BANDY", plec, ile, NVL(szefunio,0), NVL(bandzior,0), NVL(lowczy,0), NVL(lapacz, 0), NVL(kot,0), NVL(milusia,0), NVL(dzielczy,0)--, suma
+FROM
+(SELECT b.nazwa nazwa_bandy, funkcja, 
+        --DECODE(k.plec, 'D', b.nazwa, ' ') NAZWAB, 
+        DECODE(k.plec, 'D', 'Kotka', 'Kocor') plec, to_char(COUNT(k.pseudo)) ile,
+        NVL(przydzial_myszy,0)+NVL(myszy_extra,0) przydzial
+        --,SUM(NVL(przydzial_myszy,0)+NVL(myszy_extra,0)) suma 
+    FROM Kocury k JOIN Bandy b
+    ON k.nr_bandy = b.nr_bandy
+    GROUP BY b.nazwa, k.plec--, funkcja, przydzial_myszy, myszy_extra
+)
+PIVOT
+    (SUM (przydzial)
+    FOR funkcja
+    IN ('SZEFUNIO' szefunio,
+        'BANDZIOR' bandzior,
+        'LOWCZY' lowczy,
+        'LAPACZ' lapacz,
+        'KOT' KOT,
+        'MILUSIA' milusia,
+        'DZIELCZY' dzielczy)
+    )
+ ORDER BY nazwa_bandy;
+ 
+ 
+ 
+ select * from
+              (select nazwa, decode(PLEC, 'D', 'Kotka' , 'Kocur') plec, FUNKCJA, (NVL(PRZYDZIAL_MYSZY,0) + nvl(MYSZY_ExTRA, 0)) przydzial
+                from KOCURY natural join BANDY K1)
+pivot (
+    sum(NVL(przydzial,0))
+    for FUNKCJA
+    IN ('SZEFUNIO', 'BANDZIOR', 'LOWCZY', 'LAPACZ', 'KOT', 'MILUSIA', 'DZIELCZY')
+    )
+union
+select * from
+              (select 'Zjada razem ' nazwa, ' ' plec, FUNKCJA, (NVL(PRZYDZIAL_MYSZY,0) + nvl(MYSZY_ExTRA, 0)) przydzial
+              from KOCURY natural join BANDY)
+pivot (
+    sum(NVL(przydzial,0))     --element agregacji (funkcja wyliczeniowa)
+    for FUNKCJA                       --for X dane na podstawie ktorych maj¹ powstac kolumny
+    IN ('SZEFUNIO', 'BANDZIOR', 'LOWCZY', 'LAPACZ', 'KOT', 'MILUSIA', 'DZIELCZY') --in (y1,y2) jakie kolumny maj¹ powstaæ na podstawie danych z kolumny X
+    );
