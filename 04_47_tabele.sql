418008a87f8cc78e357cc8a0467e65156bee3a1f
Monika Marusiak <moniusiar@gmail.com>
2019-12-27 18:19:53 +0100

    created types and tables

--- 04_47_tabele.sql
+++ 04_47_tabele.sql
@@ -3,25 +3,23 @@ CREATE TABLE Kocury2 OF KOCURY_O
 (imie NOT NULL, 
  CONSTRAINT koc2_pk PRIMARY KEY (pseudo),
  CONSTRAINT koc2_plec_ch CHECK (plec IN ('M', 'D')));
+-- DROP TABLE Kocury2;
  
 CREATE TABLE Plebs OF PLEBS_o
- (imie NOT NULL,
- szef SCOPE IS Kocury2,
- CONSTRAINT plebs_pk PRIMARY KEY (pseudo),
- CONSTRAINT plebs_plec_ch CHECK (plec IN ('M', 'D')));
+ ( kot SCOPE IS Kocury2,
+ CONSTRAINT plebs_fk FOREIGN KEY(pseudo) REFERENCES Kocury2(pseudo),
+ CONSTRAINT plebs_pk PRIMARY KEY (pseudo));
 -- DROP TABLE plebs;
  
 CREATE TABLE Elita OF ELITA_O
- (imie NOT NULL,
- szef SCOPE IS Kocury2,
- sluga SCOPE IS Plebs,
- CONSTRAINT elita_pk PRIMARY KEY (pseudo),
- CONSTRAINT elita_plec_ch CHECK (plec IN ('M', 'D')));
+  ( kot SCOPE IS Kocury2,
+ CONSTRAINT elita_fk FOREIGN KEY(pseudo) REFERENCES Kocury2(pseudo),
+ CONSTRAINT elita_pk PRIMARY KEY (pseudo));
 -- DROP TABLE elita;
 
 CREATE TABLE Konto OF KONTO_MYSZY_O
 (data_wprowadzenia NOT NULL,
- wlasciciel NOT NULL,
+ wlasciciel SCOPE IS Elita,
  CONSTRAINT konto_pk PRIMARY KEY(nr_myszy));
 -- DROP TABLE Konto;
  
