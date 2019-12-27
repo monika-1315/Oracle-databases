0daf08fa75fbe3867e053d9327c5148efea15e1f
Monika Marusiak <moniusiar@gmail.com>
2019-12-27 22:41:51 +0100

    inserted kocury's data

--- 04_47_insert.sql
+++ 04_47_insert.sql
@@ -2,18 +2,36 @@ ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
 
 
 DECLARE
+szef REF Kocury_o;
+cnt NUMBER(2);
 CURSOR koty IS SELECT * FROM Kocury 
                 CONNECT BY PRIOR pseudo=szef
                 START WITH szef IS NULL;
---dyn_sql VARCHAR2(10000);
+dyn_sql VARCHAR2(10000);
 BEGIN
     FOR kot IN koty
     LOOP
-      EXECUTE IMMEDIATE 'INSERT INTO Kocury2
-                    SELECT ''' || kot.imie || ''', ''' || kot.plec || ''', ''' || kot.pseudo || ''', ''' || kot.funkcja 
+    szef:=NULL;
+    SELECT COUNT(*) INTO cnt FROM Kocury2 P WHERE P.pseudo=kot.szef;
+    IF (cnt>0) THEN
+        SELECT REF(P) INTO szef FROM Kocury2 P WHERE P.pseudo=kot.szef;
+    END IF;
+      --EXECUTE IMMEDIATE 
+      dyn_sql:='DECLARE
+szef REF Kocury_o;
+cnt NUMBER(2);
+BEGIN
+szef:=NULL;
+    SELECT COUNT(*) INTO cnt FROM Kocury2 P WHERE P.pseudo='''|| kot.szef||''';
+    IF (cnt>0) THEN
+        SELECT REF(P) INTO szef FROM Kocury2 P WHERE P.pseudo='''|| kot.szef||''';
+    END IF;
+INSERT INTO Kocury2 VALUES
+                    (Kocury_O(''' || kot.imie || ''', ''' || kot.plec || ''', ''' || kot.pseudo || ''', ''' || kot.funkcja 
                     || ''',''' ||kot.w_stadku_od || ''',''' || kot.przydzial_myszy ||''',''' || kot.myszy_extra ||
-                        ''',''' || kot.nr_bandy || ''', REF(K) FROM Kocury2 K WHERE pseudo=''' || kot.szef || '''';
-       -- DBMS_OUTPUT.PUT_LINE(dyn_sql);
+                        ''',''' || kot.nr_bandy || ''', ' || 'szef' || '));END;';
+       DBMS_OUTPUT.PUT_LINE(dyn_sql);
+       EXECUTE IMMEDIATE  dyn_sql;
     END LOOP;
     --COMMIT;
 EXCEPTION
