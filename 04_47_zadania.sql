94bf7f54892c6891af5f60aa8d0feeeb752a1e59
Monika Marusiak <moniusiar@gmail.com>
2019-12-28 14:18:41 +0100

    tasks from list 2

--- 04_47_zadania.sql
+++ 04_47_zadania.sql
@@ -1,3 +1,5 @@
+ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
+
 SELECT  pseudo
  FROM (SELECT K.pseudo pseudo FROM Kocury2 K ORDER BY K.Dochod_myszowy() ASC)
          WHERE ROWNUM<= (SELECT COUNT(*) FROM Kocury2)/2;
@@ -13,3 +15,19 @@ SELECT K.kot.nr_bandy nr_bandy, COUNT(K.pseudo) ilosc_kotow_w_elicie
     FROM Elita K
     GROUP BY K.kot.nr_bandy
     ORDER BY K.kot.nr_bandy;
+    
+--lista 2
+--Zad18
+SELECT k1.imie, k1.w_stadku_od "POLUJE OD"
+    FROM Kocury2 k1, Kocury2 k2
+   -- ON k1.pseudo = k2.pseudo 
+    WHERE k2.imie='JACEK' AND k1.w_stadku_od < k2.w_stadku_od
+    ORDER BY 2 DESC;
+--Zad22
+SELECT k.funkcja, k.pseudo, sel.lw "Liczba wrogow"
+FROM Kocury2 k JOIN
+(SELECT COUNT(pseudo) lw, pseudo
+    FROM Incydenty
+    GROUP BY pseudo) sel
+ON k.pseudo = sel.pseudo
+WHERE sel.lw>1;
