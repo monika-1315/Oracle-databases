7ae1ed6f09974e77d53ede72021bb7c606efe72e
Monika Marusiak <moniusiar@gmail.com>
2020-01-11 01:52:39 +0100

    working second procedure

--- 04_49.sql
+++ 04_49.sql
@@ -3,15 +3,16 @@ BEGIN
         nr_myszy NUMBER(7) CONSTRAINT myszy_pk PRIMARY KEY,
         lowca VARCHAR2(15) CONSTRAINT m_lowca_fk REFERENCES Kocury(pseudo),
         zjadacz VARCHAR2(15) CONSTRAINT m_zjadacz_fk REFERENCES Kocury(pseudo),
-        waga_myszy NUMBER(3) CONSTRAINT waga_myszy_ogr CHECK (waga_myszy BETWEEN 10 AND 120),
+        waga_myszy NUMBER(3) CONSTRAINT waga_myszy_ogr CHECK (waga_myszy BETWEEN 1 AND 10),
         data_zlowienia DATE DEFAULT SYSDATE,
-        data_wydania DATE,
-         CONSTRAINT daty_popr CHECK (data_zlowienia <= data_wydania))';
+        data_wydania DATE)';--,
+        -- CONSTRAINT daty_popr CHECK (data_zlowienia <= data_wydania))';
 EXCEPTION
     WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM);
 END;
 /
---DROP TABLE myszy;
+DROP TABLE myszy;
+DELETE FROM myszy;
 CREATE SEQUENCE nr_myszy_myszy;
 
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
@@ -25,9 +26,9 @@ DECLARE
     tab_pseudo tp:=tp();
     TYPE tm IS TABLE OF NUMBER(3);
     tab_myszy tm:=tm();
-    TYPE myszy_rek IS TABLE OF Myszy%ROWTYPE INDEX BY PLS_INTEGER;
+    TYPE myszy_rek IS TABLE OF Myszy%ROWTYPE INDEX BY BINARY_INTEGER;
     myszki myszy_rek;
-    nr_myszy NUMBER(7):=0;
+    nr_myszy BINARY_INTEGER:=0;
     ind_zjadacza NUMBER(2);
     lw NUMBER;
 BEGIN
@@ -38,7 +39,7 @@ BEGIN
         SELECT pseudo, NVL(przydzial_myszy,0)+NVL(myszy_extra, 0)
             BULK COLLECT INTO tab_pseudo, tab_myszy
             FROM Kocury WHERE w_stadku_od<ost_sroda;
-        DBMS_OUTPUT.PUT_LINE(myszy_mies);
+        --DBMS_OUTPUT.PUT_LINE(myszy_mies);
         ind_zjadacza:=1;
         myszy_mies:=CEIL(myszy_mies/tab_pseudo.COUNT);
         FOR i IN 1..(myszy_mies*tab_pseudo.COUNT)
@@ -56,7 +57,7 @@ BEGIN
                 ind_zjadacza:= DBMS_RANDOM.VALUE(1, tab_myszy.COUNT);
             END IF;
             myszki(nr_myszy).zjadacz := tab_pseudo(ind_zjadacza);
-            myszki(nr_myszy).waga_myszy := DBMS_RANDOM.VALUE(10,120);
+            myszki(nr_myszy).waga_myszy := DBMS_RANDOM.VALUE(1,10);
             myszki(nr_myszy).data_zlowienia := pierwszy_dzien+DBMS_RANDOM.VALUE(0,TRUNC(ost_sroda)-TRUNC(pierwszy_dzien)) ;
             IF (ost_sroda != ost_dzien) THEN
                 myszki(nr_myszy).data_wydania := ost_sroda;
@@ -64,29 +65,29 @@ BEGIN
         END LOOP;
         
         pierwszy_dzien:=ost_sroda+1;
-        ost_sroda:= NEXT_DAY(LAST_DAY(pierwszy_dzien) - 7, 'Œroda');
+        ost_sroda:= NEXT_DAY(LAST_DAY(ADD_MONTHs(pierwszy_dzien,1)) - 7, 'Œroda');
         IF ost_sroda>ost_dzien THEN
             ost_sroda:=ost_dzien;
         END IF;
     END LOOP zew;
-        DBMS_OUTPUT.PUT_LINE(myszki.COUNT);
+       -- DBMS_OUTPUT.PUT_LINE(myszki.COUNT);
         FORALL i IN 1..myszki.COUNT SAVE EXCEPTIONS 
             INSERT INTO Myszy(nr_myszy, lowca, zjadacz, waga_myszy,data_zlowienia, data_wydania) 
             VALUES(myszki(i).nr_myszy, myszki(i).lowca, myszki(i).zjadacz, myszki(i).waga_myszy, myszki(i).data_zlowienia, myszki(i).data_wydania);
---        lw:=SQL%BULK_EXCEPTIONS.COUNT;
---       FOR i IN 1..lw
---       LOOP
---         DBMS_OUTPUT.PUT_LINE('Blad '||i||': myszka '||
---           SQL%BULK_EXCEPTIONS(i).error_index||' - '||
---           SQLERRM(-SQL%BULK_EXCEPTIONS(i).error_code));
---       END LOOP;
-
-            
---EXCEPTION
---    WHEN OTHERS THEN DBMS_OUTPUT.PUT_LINE(SQLERRM);
+    
+EXCEPTION
+    WHEN OTHERS THEN 
+    lw:=SQL%BULK_EXCEPTIONS.COUNT;
+         DBMS_OUTPUT.PUT_LINE(lw);
+       FOR i IN 1..lw
+       LOOP
+         DBMS_OUTPUT.PUT_LINE('Blad '||i||': myszka '||
+           SQL%BULK_EXCEPTIONS(i).error_index||' - '||
+           SQLERRM(-SQL%BULK_EXCEPTIONS(i).error_code));
+       END LOOP;
+       DBMS_OUTPUT.PUT_LINE(SQLERRM);
 END;
 /
-show errors;
 --
 --DECLARE
 --TYPE tp IS TABLE OF Kocury.pseudo%TYPE;
@@ -105,5 +106,5 @@ show errors;
 --    END;
 --    /
 
-SELECT * FROM Myszy;
+SELECT  * FROM Myszy WHERE EXTRACT(YEAR FROM DATA_WYDANIA)=2019;
 DELETE FROM Myszy;
